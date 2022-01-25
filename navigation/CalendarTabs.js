e5600f12c6b38a1afd8e7ecbb3501fb58ae2cc86
suriming <soolim520@gmail.com>
2022-01-25 20:09:13 +0900

    파이어베이스 건드리기전

--- navigation/CalendarTabs.js
+++ navigation/CalendarTabs.js
@@ -1,14 +1,25 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Calendar from '../screens/Calendar';
 import FriendList from "../screens/FriendList";
-
+import Archive from "../screens/Archive";
+import { YELLOW_BASIC } from "../colors";
 const Tab = createMaterialTopTabNavigator();
 
 function CalendarTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Calendar" component={Calendar} />
-      <Tab.Screen name="FriendList" component={FriendList} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: { fontSize:16 },
+        tabBarActiveTintColor: YELLOW_BASIC,
+        tabBarInactiveTintColor: "#808080",
+        tabBarIndicatorStyle: {
+          backgroundColor: YELLOW_BASIC
+        }
+      }}
+    >
+      <Tab.Screen name="캘린더" component={Calendar} />
+      <Tab.Screen name="아카이브" component={Archive} />
+      <Tab.Screen name="친구" component={FriendList} />
     </Tab.Navigator>
   )
 }
---
+++ screens/Archive.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Archive = () => (
+    <View style={{flex:1, justifyContent: "center", alignItems:"center"}}>
+        <Text>Archive</Text>
+    </View>
+)
+
+export default Archive;
